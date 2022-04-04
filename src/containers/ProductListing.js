0ec64bc5b767976563e6da781bbc1b852cc9af8e
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-04-04 17:30:46 +0500

    updated

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import ProductComponent from "./ProductComponent";
+import { useDispatch } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
+
+const ProductListing = () => {
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const res = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => console.log(err));
+
+    dispatch(setProducts(res.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductListing;
